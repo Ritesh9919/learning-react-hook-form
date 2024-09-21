62ea40790ea80bf605de51582ed30084cf1d566b
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-21 16:46:02 +0530

    managing form state

--- src/components/YouTubeFrom.jsx
+++ src/components/YouTubeFrom.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 
 function YouTubeFrom() {
   const form = useForm();
+  const { register } = form;
   return (
     <form className="p-5 shadow-lg w-[300px] h-[350px] mx-auto mt-10 flex flex-col gap-7 items-center justify-center rounded-md">
       <div className="flex gap-3 justify-center items-center">
@@ -13,6 +14,7 @@ function YouTubeFrom() {
           type="text"
           id="username"
           name="username"
+          {...register("username")}
           placeholder="Username"
           className="shadow-md p-2 rounded-md outline-none"
         />
@@ -25,6 +27,7 @@ function YouTubeFrom() {
           type="text"
           id="email"
           name="email"
+          {...register("email")}
           placeholder="Email"
           className="shadow-md p-2 rounded-md outline-none"
         />
@@ -37,6 +40,7 @@ function YouTubeFrom() {
           type="text"
           id="channel"
           name="channel"
+          {...register("channel")}
           placeholder="Channel"
           className="shadow-md p-2 rounded-md outline-none"
         />
